16391f170f4336a92c05d5fbc06935e453773584
aircampro <147149143+aircampro@users.noreply.github.com>
2026-04-18 16:33:35 +0100

    Update realsensecv.py

--- src/realsensecv.py
+++ src/realsensecv.py
@@ -1,17 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Library for Realsense Camera
+#
 import pyrealsense2 as rs
 import numpy as np
 
 class RealsenseCapture:
 
-    def __init__(self):
+    def __init__(self, d=1):
         self.WIDTH = 640
         self.HEGIHT = 480
         self.FPS = 30
-        # Configure depth and color streams
+        # Configure depth and color streams and decimate filter 
         self.config = rs.config()
         self.config.enable_stream(rs.stream.color, self.WIDTH, self.HEGIHT, rs.format.bgr8, self.FPS)
         self.config.enable_stream(rs.stream.depth, self.WIDTH, self.HEGIHT, rs.format.z16, self.FPS)
-
+        self.dec_val = d
+        self.decimate = rs.decimation_filter() 
+        self.decimate.set_option(rs.option.filter_magnitude, 2 ** self.dec_val)
+        
     def start(self):
         # Start streaming with a check to see that the camera actually has a RGB color sensor if not we exit
         self.pipeline = rs.pipeline()
@@ -30,16 +38,23 @@ class RealsenseCapture:
             exit(0)        
         self.pipeline.start(self.config)
         print('pipline start')
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
 
-    def read(self, is_array=True):
+    def read(self, is_array=True, dec=False):
         # Flag capture available
         ret = True
         # get frames
         frames = self.pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+        self.aligend_depth_frame = aligned_frames.get_depth_frame()
+        self.aligend_color_frame = aligned_frames.get_color_frame()
+        self.aligend_depth_frame_distance = self.aligend_depth_frame.as_depth_frame()
         # separate RGB and Depth image
-        self.color_frame = frames.get_color_frame()  # RGB
-        self.depth_frame = frames.get_depth_frame()  # Depth
-
+        self.color_frame = frames.get_color_frame()                                                     # RGB
+        self.depth_frame = frames.get_depth_frame()                                                     # Depth
+        if dec == True:                                                                                 # dec == True for decimate filter
+            self.depth_frame = self.decimate.process(self.depth_frame)
         if not self.color_frame or not self.depth_frame:
             ret = False
             return ret, (None, None)
